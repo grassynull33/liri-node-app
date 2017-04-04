dc3e37a405bbc26d442e83eb4b6e12cdaa2fce6c
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-04-03 20:18:26 -0700

    npm init and package.json

--- liri.js
+++ liri.js
@@ -1,3 +1,15 @@
 var keys = require("./keys.js");
 
 var twitterCredentials = keys.twitterKeys;
+
+var command = process.argv[2];
+
+if(command === "my-tweets") {
+
+} else if(command === "spotify-this-song") {
+
+} else if(command === "movie-this") {
+
+} else if(command === "do-what-it-says") {
+	
+}
